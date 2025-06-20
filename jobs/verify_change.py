e94a30aed64a62b3f56a0d8ebb090af13470636b
R.K. Oliver <robert.k.oliver@gmail.com>
2025-06-20 15:42:30 -0400

    Discontinue use of requirements.txt

--- jobs/verify_change.py
+++ jobs/verify_change.py
@@ -2,18 +2,41 @@
 #
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
+import shutil
+from pathlib import Path
 
 # Dog-fooding: verify-change job
-from rkojob import context_value, lazy_action, lazy_format
+from rkojob import context_value, lazy_action, lazy_format, JobContext
 from rkojob.actions import ToolActionBuilder, VerifyPythonTestStructure
 from rkojob.job import JobBuilder
 
+pip = ToolActionBuilder("pip")
 tox = ToolActionBuilder("tox")
 
 with JobBuilder("verify-change") as job:
 
     with job.stage("setup") as setup:
-        pass
+        with setup.step("clean") as clean:
+            def clean_action(context: JobContext) -> None:
+                to_be_deleted: list[Path] = [Path(path) for path in (".tox", "build", "dist")]
+                to_be_deleted.extend(Path().glob("**/*.egg-info"))
+                for to_delete in to_be_deleted:
+                    context.events.start_item(f"Deleting {to_delete}")
+                    if to_delete.is_dir():
+                        shutil.rmtree(to_delete)
+                        outcome = "\u2705"
+                    elif to_delete.is_file():
+                        to_delete.unlink()
+                        outcome = "\u2705"
+                    elif not to_delete.exists():
+                        outcome = "\u274E"
+                    else:
+                        outcome = "\uFE0F"
+                    context.events.finish_item(outcome)
+            clean.action = clean_action
+
+        with setup.step("install-tox") as install_tox:
+            install_tox.action = pip.install("tox")
 
     with job.stage("static-analysis") as static_analysis:
         static_analysis.concurrent = True
